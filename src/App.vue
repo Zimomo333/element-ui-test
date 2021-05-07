bdcfbdf27cfcaa5e8d253d29fad53de2104041b2
Zimomo333 <lzh120908717@gmail.com>
2021-05-07 16:00:23 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>ElementUI Vue2 {{ items.length }} 条数据</h1>
+    <p>{{ action }} 费时 {{time}}ms.</p>
+    <el-button type="primary" @click="shuffle">更新</el-button>
+    <el-table :data="items">
+      <el-table-column label="静态">kkb</el-table-column>
+      <el-table-column label="静态">kkb</el-table-column>
+      <el-table-column label="静态">kkb</el-table-column>
+      <el-table-column prop="label" label="动态"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { shuffle } from 'lodash';
+var total = 500;
+var items = [];
+for (var i = 0; i < total; i++) {
+  items.push({
+    id: i,
+    label: String(Math.random()).slice(0, 5),
+  });
+}
+let s = window.performance.now();
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      total: total,
+      time: 0,
+      action: '初次渲染',
+      items: items,
+      selected: null,
+      actions: ['删除', '更新'],
+    };
+  },
+  mounted() {
+    this.time = window.performance.now() - s;
+  },
+  methods: {
+    shuffle() {
+      this.action = '更新渲染';
+      this.items = shuffle(this.items);
+      let s = window.performance.now();
+      this.$nextTick(() => {
+        this.time = window.performance.now() - s;
+      });
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
